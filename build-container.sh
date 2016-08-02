5472e46d366bf4c0ed8504d0502ad59b0dfb63e6
rbrooker <rbrooker@aetherealmind.com>
2016-08-02 14:33:38 -0400

    set version for Erlang

--- build-container.sh
+++ build-container.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 # build scripts to
 
-while getopts :d: opt; do
-  case $opt in 
-    d) 
-        TAG=$OPTARG 
-        docker build --no-cache --pull  --rm=true -t rbrooker/rabbitmq -t rbrooker/rabbitmq:${TAG} -f Dockerfile.prod .
-        ;;
-    \?) 
-        echo "try again -d TAG ... " 
-        ;;
-    :)
-         docker build -t rabbitmq -f Dockerfile.debug .
-         ;;
-     esac
-   done
+#        docker build --no-cache --pull  --rm=true -t rbrooker/rabbitmq -t rbrooker/rabbitmq:${TAG} -f Dockerfile .
+        docker build -t rbrooker/rabbitmq .  
 
-
-exit 0
+exit $?
 
